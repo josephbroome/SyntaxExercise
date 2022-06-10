47bc54ba20de268d348c7d4dbc9b488043eba70b
Joseph Broome <josephlloydbroome@gmail.com>
2022-06-09 17:04:39 -0700

    syntaxexercise

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        var answer = 9;
+        var response = (answer < 9) ? $"{answer} is less than mine" : $"{answer} is greater than mine or equal to mine";
+        Console.WriteLine(response);
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
